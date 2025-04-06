34fe547aaea2ba4f7f01e923d74adb39303283a6
AllanW178 <allanwang@riccarton.school.nz>
2025-04-07 09:02:53 +1200

    Update Actual Assessment.py

--- Actual Assessment/Actual Assessment.py
+++ Actual Assessment/Actual Assessment.py
@@ -82,7 +82,7 @@ def display_menu():
         # Return back to the main page (Main function)
         return
     
-    # This is the place where the information (eg., card names and their values) will be stored in.
+    # This is the place where the information (eg., card names and their values) will be stored in. In other words, save it into a variable.
     text = ""
     # Scan for every card and its value.
     for name, ability in cards.items():
@@ -135,42 +135,131 @@ def remove_menu():
     # Print out the success message when the chosen card has been removed in the interface.
     easygui.msgbox(f"{remove_card} has been successfully removed!", title = "SUCCESS")
 
+
+
+
+
+
+
+# [THE CODE BELOW ALLOWS THE USERS TO ADD THEIR OWN ABILITIES INSTEAD OF THE GIVEN ABILITIES (eg., Strength, Speed, Stealth & Cunning), WHICH ISN'T WHAT WE WANT; WE WANT THE USER TO ONLY ENTER THE VALUES FOR EACH OF THE GIVEN ABILITIES.]
+# [IN A NUTSHELL, THIS CODE IS DYNAMIC ESPECIALLY WHEN THE USERS WANT TO TYPE DOWN THEIR OWN ABILITIES AND VALUES.]
+
+
+# ----------------------------------------------------------------------------------------------------------
+
+# def add_menu():
+#     add_card = easygui.enterbox("Enter the card's name: ", title = "ADD PAGE")
+
+#     if not add_card:
+#         easygui.msgbox("Your input cannot be blank; please try again.", title = "NOTE")
+#         return
+
+#     if add_card in cards:
+#         easygui.msgbox(f"{add_card} is already exists; please try something else.", title = "NOTE")
+#         return
+    
+#     card[add_card] = {}
+
+#     for i in range(4):
+#         while True:
+#             abilities = easygui.enterbox("Enter an ability: ", title = "ADD PAGE")
+#             if not abilities:
+#                 easygui.msgbox("The ability column cannot be empty.", title = "NOTE")
+#             else:
+#                 break
+        
+#         while True:
+#             try:
+#                 values = easygui.enterbox(f"Enter the value (1-25): ", title = "ADD PAGE")
+#                 if not values:
+#                     easygui.msgbox("The value column cannot be empty.", title = "NOTE")
+#                 else:
+#                     values = int(values)
+#                     break
+#             except ValueError:
+#                 easygui.msgbox("Invalid value; please try to enter a number from 1 to 25.")
+        
+#         cards[add_card][abilities.title()] = values
+    
+#     easygui.msgbox(f"{add_card} has been successfully added!", title = "SUCCESS")
+
+# ----------------------------------------------------------------------------------------------------------
+
+
+
+
+
+# [THE CODE BELOW IS A FIXED VERSION FROM THE CODE ABOVE THAT ONLY ALLOWS THE USERS TO ENTER THE VALUES FOR EACH ABILITY]:
+
+# This is function where the users can add their own values for their given abilities.
 def add_menu():
-    add_card = easygui.enterbox("Enter the card's name: ", title = "ADD PAGE")
+    add_card = easygui.enterbox("Enter the card's name:", title = "ADD CARD")
 
+    add_card = add_card.title()
+
+    # If the users don't type anything down, a text will inform them and return to the main menu, which is the Main function.
     if not add_card:
-        easygui.msgbox("Your input cannot be blank; please try again.", title = "NOTE")
-        return
-
-    if add_card in cards:
-        easygui.msgbox(f"{add_card} is already exists; please try something else.", title = "NOTE")
+        easygui.msgbox("Your input cannot be empty; please try to enter a card's name.", title = "NOTE")
         return
     
-    card[add_card] = {}
+    # If user's entered card is already exists in the card list, a message will inform them to type down a different card name.
+    if add_card in cards:
+        easygui.msgbox(f"Card '{add_card}' is already exists; please try to enter a different name for your card.", title = "NOTE")
+        return
+    
 
-    for i in range(4):
-        while True:
-            abilities = easygui.enterbox("Enter an ability: ", title = "ADD PAGE")
-            if not abilities:
-                easygui.msgbox("The ability column cannot be empty.", title = "NOTE")
+    # Below are four while loops (four different abilities) to check the valiadity for user's entered values. If not, the program will tell them to try again.
+    while True:
+        try:
+            strength = int(easygui.enterbox("Enter the strength for your card (1-25): "))
+            if strength < 0 or strength > 25:
+                easygui.msgbox(f"Your strength value must be between 1-25. Currently, your value is {strength}.")
             else:
                 break
-        
-        while True:
-            try:
-                values = easygui.enterbox(f"Enter the value (1-25): ", title = "ADD PAGE")
-                if not values:
-                    easygui.msgbox("The value column cannot be empty.", title = "NOTE")
-                else:
-                    values = int(values)
-                    break
-            except ValueError:
-                easygui.msgbox("Invalid value; please try to enter a number from 1 to 25.")
-        
-        cards[add_card][abilities.title()] = values
-    
-    easygui.msgbox(f"{add_card} has been successfully added!", title = "SUCCESS")
+        except ValueError:
+            easygui.msgbox("Please enter a valid number from 1-25.")
 
+    while True:
+        try:
+            speed = int(easygui.enterbox("Enter the speed for your card (1-25): "))
+            if speed < 0 or speed > 25:
+                easygui.msgbox(f"Your strength value must be between 1-25. Currently, your value is {speed}.")
+            else:
+                break
+        except ValueError:
+            easygui.msgbox("Please enter a valid number from 1-25.")
+
+    while True:
+        try:
+            stealth = int(easygui.enterbox("Enter the stealth for your card (1-25): "))
+            if stealth < 0 or stealth > 25:
+                easygui.msgbox(f"Your stealth value must be between 1-25. Currently, your value is {stealth}.")
+            else:
+                break
+        except ValueError:
+            easygui.msgbox("Please enter a valid number from 1-25.")
+
+    while True:
+        try:
+            cunning = int(easygui.enterbox("Enter the cunning for your card (1-25) : "))
+            if cunning < 0 or cunning > 25:
+                easygui.msgbox(f"Your cunning value must be between 1-25. Currently, your value is {cunning}.")
+            else:
+                break
+        except ValueError:
+            easygui.msgbox("Please enter a valid number from 1-25.")
+
+    #After the users have entered the correct values for the four abilities, they will be recorded correspondingly into their dictionary.
+    cards[add_card] = {
+        'Strength': strength,
+        'Speed': speed,
+        'Stealth': stealth,
+        'Cunning': cunning
+    }
+
+    # This is a final message to inform the users that their card has been added successfully. 
+    easygui.msgbox(f"Your card '{add_card}' has been successfully added!", title = "SUCCESS")
+    
 
 # This is a main menu, the user can manipulate all the functions here (eg., Display Card, Search Card, Remove Card, Add Card, Modify Card & Exit the program).
 def main():
